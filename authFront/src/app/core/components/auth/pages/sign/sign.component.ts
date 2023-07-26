a48ccf864c383625ae4f7d82e7437b859b33d8da
Henrique <103683674+Henrique-PS@users.noreply.github.com>
2023-07-26 18:28:54 -0300

    137. Hands on 8

--- authFront/src/app/core/components/auth/pages/sign/sign.component.ts
+++ authFront/src/app/core/components/auth/pages/sign/sign.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign',
   templateUrl: './sign.component.html',
-  styleUrls: ['./sign.component.scss']
+  styleUrls: ['./sign.component.scss'],
 })
 export class SignComponent {
+  public formAuth: FormGroup = this.formBuilder.group({
+    email: [''/* valor do campo */, [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  })
 
+  public submitForm(){
+    if(this.formAuth.valid){
+      console.log('op')
+    }
+  }
+
+  constructor(private formBuilder: FormBuilder) {}
 }
