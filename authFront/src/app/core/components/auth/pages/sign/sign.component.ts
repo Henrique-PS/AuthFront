86db8d354ff027b0cdde0bd53860a3ea5543031f
Henrique <103683674+Henrique-PS@users.noreply.github.com>
2023-07-26 19:37:34 -0300

    138. Hands on 9

--- authFront/src/app/core/components/auth/pages/sign/sign.component.ts
+++ authFront/src/app/core/components/auth/pages/sign/sign.component.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// Services
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-sign',
   templateUrl: './sign.component.html',
   styleUrls: ['./sign.component.scss'],
 })
-export class SignComponent {
+export class SignComponent implements OnInit {
   public formAuth: FormGroup = this.formBuilder.group({
-    email: [''/* valor do campo */, [Validators.required, Validators.email]],
-    password: ['', [Validators.required]]
-  })
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 
-  public submitForm(){
-    if(this.formAuth.valid){
-      console.log('op')
+  public msgError!: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {}
+
+  public submitForm() {
+    if (this.formAuth.valid) {
+      this.authService
+        .sign({
+          email: this.formAuth.value.email,
+          password: this.formAuth.value.password,
+        })
+        .subscribe({
+          next: (res) => res,
+          error: (e) => (this.msgError = e),
+        });
     }
   }
-
-  constructor(private formBuilder: FormBuilder) {}
 }
