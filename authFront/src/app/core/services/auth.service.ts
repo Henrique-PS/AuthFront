ae8f581de65a70ec907d1854a5aa6264ad140d62
Henrique <103683674+Henrique-PS@users.noreply.github.com>
2023-07-26 22:45:55 -0300

    140. Hands on 11

--- authFront/src/app/core/services/auth.service.ts
+++ authFront/src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, catchError, map, throwError } from 'rxjs';
 
 @Injectable({
@@ -8,18 +9,28 @@ import { Observable, catchError, map, throwError } from 'rxjs';
 export class AuthService {
   private url: string = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   public sign(payLoad: { email: string; password: string }): Observable<any> {
-    return this.http.post(`${this.url}/sign`, payLoad).pipe(
+    return this.http.post<{ token: string }>(`${this.url}/sign`, payLoad).pipe(
       map((res) => {
-        return console.log(res);
+        localStorage.removeItem('accsses_token')
+        localStorage.setItem('accsses_token', res.token)
+        return this.router.navigate(['admin'])
       }),
       catchError((e) => {
-        if(e.error.message) return throwError(() => e.error.message);
+        if (e.error.message) return throwError(() => e.error.message);
 
-        return throwError(() => "No momento não estamos conseguindo validar estes dados, tente novamente mais tarde");   
+        return throwError(
+          () =>
+            'No momento não estamos conseguindo validar estes dados, tente novamente mais tarde'
+        );
       })
     );
+  }
+
+  public logout(){
+    localStorage.removeItem('accsses_token')
+    return this.router.navigate([''])
   }
 }
